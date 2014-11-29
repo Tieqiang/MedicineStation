bf8014bee6bc6c062204949e45bd3361bd3fc58f
sf201 <shuaifei201@163.com>
2014-11-30 00:07:08 +0800

    增加首页医嘱子页面（model.js）

---
+++ src/main/webapp/js/main/models/mr/mr-orders-model.js
@@ -0,0 +1,46 @@
+/**
+ * Created by sf201 on 2014/11/29.
+ */
+var order = function(){
+    //行号
+    this.rowNo = "";
+    //长临标志
+    this.repeateTemp = "";
+    //医嘱类别
+    this.orderClass = "";
+    //医嘱下达日期
+    this.startOrderTime = "";
+    //医嘱内容
+    this.orderText = "";
+    //剂量
+    this.dosage = "";
+    //单位
+    this.dosageUnits = "";
+    //途径
+    this.administration = "";
+    //频次
+    this.frequency = "";
+    //执行时间
+    this.performSchedule = "";
+    //停止时间
+    this.stopDateTime = "";
+    //医生说明
+    this.freqDetail = "";
+    //计价属性
+    this.billingAtrr = "";
+    //摆药
+    this.drugBillingAttrBaiyao = "";
+    //下达医生
+    this.startOrderDoctor = "";
+    //停止医生
+    this.stopOrderDoctor = "";
+    //护士
+    this.nurse = "";
+    //持续时间
+    this.durations = "";
+    //持续时间单位
+    this.durationUnit = "";
+    //转抄时间
+    this.processingTime = "";
+
+}
